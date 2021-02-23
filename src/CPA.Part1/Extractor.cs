b596b1e61a626826bed52b2201adb116eaa7b81c
Von Tan <von.aerich@gmail.com>
2021-02-23 20:50:43 +1100

    Add Extractor

---
+++ src/CPA.Part1/Extractor.cs
@@ -0,0 +1,35 @@
+﻿using CPA.Part1.Models;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace CPA.Part1
+{
+    public class Extractor
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger _logger;
+
+        public Extractor(IHttpClientFactory httpClientFactory, ILogger logger = null)
+        {
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+        }
+
+        public async Task<IEnumerable<SubjectResult>> FetchResults()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync("api/results");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                _logger?.LogError($"Cannot fetch results. Status Code - {response.StatusCode}");
+                return new List<SubjectResult>();
+            }
+
+            return await response.Content.ReadFromJsonAsync<IEnumerable<SubjectResult>>();
+        }
+    }
+}
---
+++ test/CPA.Part1.Tests/ExtractorTests.cs
@@ -0,0 +1,79 @@
+﻿using CPA.Part1.Models;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CPA.Part1.Tests
+{
+    public class ExtractorTests
+    {
+        [Fact]
+        public async Task CanFetchResultsFromApi()
+        {
+            var httpClient = CreateHttpClient(new FakeHttpMessageHandler());
+            var httpClientFactory = Substitute.For<IHttpClientFactory>();
+            httpClientFactory.CreateClient(Arg.Any<string>()).Returns(httpClient);
+
+            var results = await new Extractor(httpClientFactory).FetchResults();
+
+            Assert.NotEmpty(results);
+            Assert.Contains(results, result => result.Subject == "Subject1");
+        }
+
+        [Fact]
+        public async Task ReturnsEmptyListWhenFaulty()
+        {
+            var httpClient = CreateHttpClient(new FaultyMessageHandler());
+            var httpClientFactory = Substitute.For<IHttpClientFactory>();
+            httpClientFactory.CreateClient(Arg.Any<string>()).Returns(httpClient);
+
+            var results = await new Extractor(httpClientFactory).FetchResults();
+
+            Assert.Empty(results);
+        }
+
+        private static HttpClient CreateHttpClient(HttpMessageHandler handler)
+        {
+            return new HttpClient(handler)
+            {
+                BaseAddress = new Uri("http://something")
+            };
+        }
+    }
+
+    public class FakeHttpMessageHandler : HttpMessageHandler
+    {
+        private static IEnumerable<SubjectResult> Result =>
+            new List<SubjectResult>
+            {
+                new SubjectResult
+                {
+                    Subject = "Subject1",
+                    Results = new[] { new Result { Year = 2020, Grade = "Pass" } }
+                }
+            };
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(JsonSerializer.Serialize(Result))
+            });
+        }
+    }
+
+    public class FaultyMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new HttpResponseMessage { StatusCode = HttpStatusCode.InternalServerError });
+        }
+    }
+}
