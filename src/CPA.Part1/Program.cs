1f23fba44299edcc785c13e15648563fee09c2e8
Von Tan <von.aerich@gmail.com>
2021-02-23 15:33:42 +1100

    Part 1 skeleton

---
+++ src/CPA.Part1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CPA.Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test/CPA.Part1.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CPA.Part1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
